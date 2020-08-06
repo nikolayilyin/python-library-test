452c644e2b0493f89413cf44a1d1de3f3091c216
Nikolay Ilyin <irishwithaxe@gmail.com>
2020-08-06 23:13:03 +0300

    Update library.py

--- library.py
+++ library.py
@@ -2,3 +2,5 @@ def get_output_path_from_s3_url(s3_url):
   return s3_url.strip().replace("s3.us-east-2.amazonaws.com/beam-outputs/index.html#", "beam-outputs.s3.amazonaws.com/")
   
   
+def print_things(data):
+  print('the thing is:', data)
